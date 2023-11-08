4d3e45b22564d8c1091b30952d539cf360c18262
Jas667 <100931408+Jas667@users.noreply.github.com>
2023-11-08 11:53:43 +0000

    update package and port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const { standardLimiter } = require("./middleware/rateLimiter");
 dotenv.config();
 
 const app = express();
-const PORT = process.env.APP_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
