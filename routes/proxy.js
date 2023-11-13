0e51a2f47c8ff1a6766c3a3aee53ff1e03cf15a7
Jas667 <100931408+Jas667@users.noreply.github.com>
2023-11-13 09:58:39 +0000

    add proxy test route

--- routes/proxy.js
+++ routes/proxy.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/ip', (request, response) => response.send(request.ip))
+router.get('/', (request, response) => response.send(request.ip))
 
 module.exports = router;
